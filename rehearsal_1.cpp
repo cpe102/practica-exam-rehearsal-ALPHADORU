8f52cf5b687b9e876d2c5d1befc3023521636e91
lab314-24@labeng.local <lab314-24@labeng.local>
2020-01-10 15:25:20 +0700

    Practice Exam_1

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
 
+int main()
+{
+    string N;
+    double G;
+    cout << "What is your name?: ";
+    cin >> N;
+    cout << "What is your GPA?: ";
+    cin >> G;
+    if(G >= 3.5){
+        cout << N << "InW Jrim Jrim!!!";
+    }else{
+        cout << "Try harder, " << N << "!!!";
+    }
+}
